b3dfa540ac119d1363a203d90921a6139c5e2032
Christ Doesselaere <67807770+christdoesselaere@users.noreply.github.com>
2021-04-07 16:47:37 +0200

    Implemented apikey and weather data retrieval

--- src/App.js
+++ src/App.js
@@ -1,42 +1,61 @@
-import React from 'react';
+import React, {useState} from 'react';
 import SearchBar from './components/searchBar/SearchBar';
 import TabBarMenu from './components/tabBarMenu/TabBarMenu';
 import MetricSlider from './components/metricSlider/MetricSlider';
 import './App.css';
+import axios from "axios";
+
+const apiKey = '69c4d21b7323149c13fbf6cb91f01341';
 
 function App() {
-  return (
-    <>
-      <div className="weather-container">
+    const [weatherData, setWeatherData] = useState(null);
 
-        {/*HEADER -------------------- */}
-        <div className="weather-header">
-          <SearchBar/>
+    async function fetchData() {
+        try {
+            const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=utrecht,nl&appid=${apiKey}&lang=nl`);
+            setWeatherData(result.data);
+            console.log(result.data);
+        } catch (e) {
+            console.log(e);
+        }
+    }
 
-          <span className="location-details">
-            <h2>Bewolkt</h2>
-            <h3> </h3>
-            <h1>14 &deg;</h1>
+    return (
+        <>
+            <div className="weather-container">
 
-            <button type="button">
+                {/*HEADER -------------------- */}
+                <div className="weather-header">
+                    <SearchBar/>
+
+                    <span className="location-details">
+                        {weatherData &&
+                        <>
+                            <h2>{weatherData.weather[0].description}</h2>
+                            <h3>{weatherData.name}</h3>
+                            <h1>{weatherData.main.temp}&deg;</h1>
+                        </>
+                        }
+
+                        <button type="button" onClick={fetchData}>
               Haal data op!
             </button>
           </span>
-        </div>
+                </div>
 
-        {/*CONTENT ------------------ */}
-        <div className="weather-content">
-          <TabBarMenu/>
+                {/*CONTENT ------------------ */}
+                <div className="weather-content">
+                    <TabBarMenu/>
 
-          <div className="tab-wrapper">
-            Alle inhoud van de tabbladen komt hier!
-          </div>
-        </div>
+                    <div className="tab-wrapper">
+                        Alle inhoud van de tabbladen komt hier!
+                    </div>
+                </div>
 
-        <MetricSlider/>
-      </div>
-    </>
-  );
+                <MetricSlider/>
+            </div>
+        </>
+    );
 }
 
 export default App;
